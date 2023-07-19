fce429669174b5a9313de02d5730940dea7a5bec
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-19 16:20:27 +0100

    Navbar setup with userButton

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 import { Menu } from 'lucide-react';
 
 const NavBar = () => {
   return (
-    <div className='flex items-center -p4'>
+    <div className='flex items-center p-4'>
       <Button variant="ghost" size="icon" className='md:hidden'>
         <Menu />
       </Button>
+      <div className='flex justify-end w-full'>
+        <UserButton afterSignOutUrl='/' />
+      </div>
     </div>
   );
 }

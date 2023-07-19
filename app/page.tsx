b94e3be3ab4ae3a2de4ac60ea0ff146f204be92f
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-19 15:11:54 +0100

    Setting up structure for project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <p className='text-6xl'>Hello ReactAIvate</p>
+    <Button variant="destructive" size="default">Click me</Button>
   )
 }

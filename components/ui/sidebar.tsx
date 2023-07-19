27aa8a01428fb6cfeb4ae7b3ab453f2ec14c2321
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-19 16:39:12 +0100

    Added routes

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { cn } from '@/lib/utils';
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from 'lucide-react';
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const monserrat = Montserrat({ weight: "600", subsets: ['latin'] });
+
+const routes = [
+  { label: 'Dashboard', href: '/dashboard', icon: LayoutDashboard, color: "text-sky-500" },
+  { label: 'Conversation', href: '/conversation', icon: MessageSquare, color: "text-violet-500" },
+  { label: 'Image Generation', href: '/image', icon: ImageIcon, color: "text-pink-700" },
+  { label: 'Video Generation', href: '/video', icon: VideoIcon, color: "text-emerald-500" },
+  { label: 'Music Generation', href: '/music', icon: MusicIcon, color: "text-orange-700" },
+  { label: 'Code Generation', href: '/code', icon: Code, color: "text-green-700" },
+  { label: 'Settings', href: '/settings', icon: Settings },
+]
 
 const Sidebar = () => {
   return (
@@ -17,6 +28,15 @@ const Sidebar = () => {
         </div>
           <h1 className={cn("text-2xl font-bold", monserrat.className)}>ReactAIvate</h1>
         </Link>
+        <div className='space-y-1'>
+          {routes.map((route) => (
+            <Link href={route.href} key={route.href} className='flex justify-start w-full p-3 text-sm font-medium transition rounded-lg cursor-pointer group hover:text-white hover:bg-white/10'>
+              <div className='flex items-center flex-1'>
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>))}
+        </div>
       </div>
     </div >
   );

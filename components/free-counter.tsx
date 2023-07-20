156cf827c55216428d4a3663a554e38ead4cb933
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-20 17:13:56 +0100

    Free counter now working

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -2,6 +2,9 @@
 import { useEffect, useState } from 'react'
 import { Card, CardContent } from './ui/card'
 import { MAX_FREE_COUNTS } from '@/constants'
+import { Progress } from '@/components/ui/progress'
+import { Button } from '@/components/ui/button'
+import { Zap } from 'lucide-react'
 
 interface FreeCounterProps {
   apiLimitCount: number
@@ -18,8 +21,12 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
       <Card className='border-0 bg-white/10'>
         <CardContent className='py-6'>
           <div className='mb-4 space-y-2 text-sm text-center text-white'>
-            <p>{apiLimitCount} / {MAX_FREE_COUNTS}</p>
+            <p>{apiLimitCount} / {MAX_FREE_COUNTS} Free Generations</p>
+            <Progress className='h-3' value={apiLimitCount / MAX_FREE_COUNTS * 100} />
           </div>
+          <Button className='w-full ' variant="premium">Upgrade
+            <Zap className='w-4 h-4 ml-2 fill-white' />
+          </Button>
         </CardContent>
       </Card>
     </div>

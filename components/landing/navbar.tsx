5b3204ae0b10a4f3c1c0a30d4114fa07fd46c022
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-21 16:40:40 +0100

    Navbar now working as expected

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -15,7 +15,17 @@ export const LandingNavbar = () => {
   const { isSignedIn } = useAuth()
   return (
     <nav className='flex items-center justify-between p-4 bg-transparent'>
-
+      <Link href="/" className='flex items-center'>
+        <div className='relative w-8 h-8 mr-4'>
+          <Image
+            fill alt="Logo" src="/logo.png" />
+        </div>
+        <h1 className={cn("text-2xl font-bold text-white", font.className)}>ReactAIavte</h1>
+      </Link>
+      <div className='flex items-center gap-x-2'>
+        <Link href={isSignedIn ? "/dashboard" : "register"} />
+        <Button variant="outline" className='rounded-full'>Get Started</Button>
+      </div>
     </nav>
   )
 }

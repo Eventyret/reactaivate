369e4de78ab04997374c7891ec7e3753775f252c
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-21 16:02:19 +0100

    Added Stripe subscription button

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -9,6 +9,7 @@ interface SubscriptionButtonProps {
 }
 export const SubscriptionButton = ({ isPro = false }: SubscriptionButtonProps) => {
   const [loading, setLoading] = useState(false);
+
   const onClick = async () => {
     try {
       setLoading(true);
@@ -22,7 +23,7 @@ export const SubscriptionButton = ({ isPro = false }: SubscriptionButtonProps) =
     }
   }
   return (
-    <Button variant={isPro ? "default" : "premium"} >
+    <Button variant={isPro ? "default" : "premium"} disabled={loading} onClick={onClick}>
       {isPro ? "Manage Subscription" : "Upgrade"}
       {!isPro && <Zap className="w-4 h-4 ml-2 fill-white" />
       }

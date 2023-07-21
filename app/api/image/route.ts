180fdfef7fd16b39f3da086b0218e7e87f1a2ee7
Simen Daehlin <simen.daehlin@strapi.io>
2023-07-21 16:15:18 +0100

    Fixed routes if pro

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,4 +1,5 @@
 import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
@@ -33,7 +34,9 @@ export async function POST(req: Request) {
       return new NextResponse("Resolution are required", { status: 400 });
     }
     const freeTrial = await checkApiLimit();
-    if (!freeTrial) {
+    const isPro = await checkSubscription();
+
+    if (!freeTrial && !isPro) {
       return new NextResponse("Free trial has expired.", { status: 403 });
     }
 
@@ -42,7 +45,9 @@ export async function POST(req: Request) {
       n: parseInt(amount, 10),
       size: resolution,
     });
-    await increaseApiLimit();
+    if (!isPro) {
+      await increaseApiLimit();
+    }
     return NextResponse.json(response.data.data);
   } catch (error: any) {
     if (error.response) {
--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,4 +1,5 @@
 import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
@@ -21,7 +22,8 @@ export async function POST(req: Request) {
       return new NextResponse("Prompt are required", { status: 400 });
     }
     const freeTrial = await checkApiLimit();
-    if (!freeTrial) {
+    const isPro = await checkSubscription();
+    if (!freeTrial && !isPro) {
       return new NextResponse("Free trial has expired.", { status: 403 });
     }
 
@@ -30,7 +32,9 @@ export async function POST(req: Request) {
         prompt_a: prompt,
       },
     });
-    await increaseApiLimit();
+    if (!isPro) {
+      await increaseApiLimit();
+    }
     return NextResponse.json(response);
   } catch (error: any) {
     console.log("[MUSIC_ERROR]: ", error);
